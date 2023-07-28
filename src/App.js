7e588ac62f3a2ea20240a6d4a69ff133cfa867d7
Him97 <adrian.hsin@gmail.com>
2023-07-28 12:05:05 +0300

    V1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Actors from "./components/Actors";
 import ActorCard from "./components/ActorCard";
-import { Container } from "react-bootstrap";
+import ActorCardPage from "./components/ActorCardPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
 
@@ -8,10 +9,13 @@ function App() {
     { "adult": false, "gender": 2, "id": 15048, "known_for_department": "Acting", "name": "Robby Benson", "original_name": "Robby Benson", "popularity": 12.661, "profile_path": "/rYcDiM9LrStdlcb80N3WAR2CsHX.jpg" };
 
   return (
-    <Container>
+    <BrowserRouter>
       <Actors />
       <ActorCard data={actorData} />
-    </Container>
+      <Routes>
+        <Route path="/actor/:id" element={<ActorCardPage/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/Actors.jsx
+++ src/components/Actors.jsx
@@ -1,7 +1,26 @@
-import { Form, ListGroup } from 'react-bootstrap';
+import { useState } from 'react';
+import { Container, Form, ListGroup } from 'react-bootstrap';
 
 export default function Actors() {
+  const [actors, setActors] = useState([]);
+
+  const handleInput = async (newSearchText) => {
+    const res = await fetch(`https://api.themoviedb.org/3/search/person?api_key=53d2ee2137cf3228aefae083c8158855&query=${newSearchText}`);
+    const data = await res.json();
+    setActors(data.results);
+  }
+
   return (
-    <Form.Control type="text" placeholder="Readonly input here..." />
+    <Container>
+      <Form.Control
+        type="text"
+        placeholder="Search Actors"
+        onChange={(e) => handleInput(e.target.value)} />
+      <ListGroup>
+        {actors.map((actor) => (
+          <ListGroup.Item>{actor.name}</ListGroup.Item>
+        ))}
+      </ListGroup>
+    </Container>
   );
 }
